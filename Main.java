60eb5c8212902eecef756d75993ac595fa383ea9
Mr. Burger <bburger@d155.org>
2020-01-14 17:26:53 +0000

    Complete example

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+       int [] abc = new int[10];
+
+       for(int i=0; i<abc.length-1; i++)
+       {
+         abc[i]=i+1;
+        
+       }
+       for(int x: abc)
+       System.out.print(x+" ");
+
+       System.out.println("\n=============");
+
+       //Shift over all slots from slot 3
+       for(int y =abc.length-2; y>=3; y--)
+       {
+         abc[y+1] = abc[y];
+         for(int x: abc)
+            System.out.print(x+ " ");
+        System.out.println();
+       }
+       abc[3]=10;
+       for(int x: abc)
+            System.out.print(x+ " ");
   }
 }
