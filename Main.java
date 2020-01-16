60420b6b60f52d74a372fb1eb73f7f35a0bd56ed
Mr. Burger <bburger@d155.org>
2020-01-16 16:33:37 +0000

    Added comments

--- Main.java
+++ Main.java
@@ -15,12 +15,12 @@ class Main {
        //Shift over all slots from slot 3
        for(int y =abc.length-2; y>=3; y--)
        {
-         abc[y+1] = abc[y];
-         for(int x: abc)
+         abc[y+1] = abc[y];//Shift - This is the only line you need to use in the for loop.  The rest were just used to show how the shift is actually working.
+         for(int x: abc) 
             System.out.print(x+ " ");
         System.out.println();
        }
-       abc[3]=10;
+       abc[3]=10;//Place new Value in correct slot
        for(int x: abc)
             System.out.print(x+ " ");
   }
